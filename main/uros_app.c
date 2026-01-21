ec894b2b30044d026eee34ab65324892734f9ac2
Lionel ORCIL <48251459+ioio2995@users.noreply.github.com>
2026-01-21 09:31:08 +0100

    Handle micro-ROS agent reconnection

--- main/uros_app.c
+++ main/uros_app.c
@@ -114,19 +114,6 @@ static void micro_ros_task(void *arg)
 {
     (void)arg;
 
-    rcl_ret_t rc = RCL_RET_OK;
-    bool scan_ready = false;
-    bool node_ready = false;
-    bool publisher_ready = false;
-    bool executor_ready = false;
-    bool timer_ready = false;
-
-    rcl_allocator_t allocator = rcl_get_default_allocator();
-    rclc_support_t support = {0};
-    rcl_node_t node = rcl_get_zero_initialized_node();
-    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
-    rcl_timer_t timer = rcl_get_zero_initialized_timer();
-
     scan_cfg.angle_min = ANGLE_MIN;
     scan_cfg.angle_inc = ANGLE_INC;
     scan_cfg.bins      = N_BINS;
@@ -134,104 +121,134 @@ static void micro_ros_task(void *arg)
     scan_cfg.range_max = 2.00f;
     scan_cfg.frame_id  = SCAN_FRAME;
 
-    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
-    RCCHECK_GOTO(rcl_init_options_init(&init_options, allocator), cleanup);
-    RCCHECK_GOTO(rcl_init_options_set_domain_id(&init_options, CONFIG_MICRO_ROS_DOMAIN_ID), cleanup);
-
-    ESP_LOGI(TAG_TASK, "Waiting for micro-ROS agent...");
-    led_status_set_state(LED_STATUS_WAITING);
-    while (rmw_uros_ping_agent(100, 5) != RMW_RET_OK) {
-        vTaskDelay(pdMS_TO_TICKS(200));
-    }
-    ESP_LOGI(TAG_TASK, "Agent detected, starting RCL init");
-
-    RCCHECK_GOTO(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator), cleanup);
-
-    rcl_node_options_t node_ops = rcl_node_get_default_options();
-    RCCHECK_GOTO(rclc_node_init_with_options(&node, CONFIG_MICRO_ROS_NODE_NAME, "", &support, &node_ops), cleanup);
-    node_ready = true;
-
-    RCCHECK_GOTO(rclc_publisher_init_default(
-        &publisher,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
-        CONFIG_MICRO_ROS_TOPIC_NAME),
-        cleanup);
-    publisher_ready = true;
-
-    int configured_max = max_bin_index();
-    if (configured_max >= scan_cfg.bins) {
-        ESP_LOGE(TAG_TASK, "TOF bin map exceeds scan bins (max=%d bins=%d)",
-                 configured_max, scan_cfg.bins);
-        led_status_set_state(LED_STATUS_ERROR);
-        goto cleanup;
-    }
-
-    if (!scan_builder_init(&scan_msg, &scan_cfg)) {
-        ESP_LOGE(TAG_TASK, "scan_builder_init() failed (malloc?)");
-        led_status_set_state(LED_STATUS_ERROR);
-        goto cleanup;
-    }
-    scan_ready = true;
-
-    RCCHECK_GOTO(rclc_timer_init_default2(
-        &timer,
-        &support,
-        RCL_MS_TO_NS(TIMER_PERIOD_MS),
-        timer_callback,
-        true),
-        cleanup);
-    timer_ready = true;
-
-    RCCHECK_GOTO(rclc_executor_init(&executor, &support.context, NUMBER_OF_HANDLES, &allocator), cleanup);
-    executor_ready = true;
-    RCCHECK_GOTO(rclc_executor_add_timer(&executor, &timer), cleanup);
-
-    ESP_LOGI(TAG_TASK, "micro-ROS up: node=%s topic=%s period=%dms",
-             CONFIG_MICRO_ROS_NODE_NAME, CONFIG_MICRO_ROS_TOPIC_NAME, TIMER_PERIOD_MS);
-    led_status_set_state(LED_STATUS_CONNECTED);
-
     while (true) {
-        rcl_ret_t spin_ret = rclc_executor_spin_some(&executor, RCL_MS_TO_NS(50));
-        if (spin_ret != RCL_RET_OK) {
-            ESP_LOGE(TAG_TASK, "rclc_executor_spin_some() failed");
+        rcl_ret_t rc = RCL_RET_OK;
+        bool scan_ready = false;
+        bool node_ready = false;
+        bool publisher_ready = false;
+        bool executor_ready = false;
+        bool timer_ready = false;
+        bool init_options_ready = false;
+
+        rcl_allocator_t allocator = rcl_get_default_allocator();
+        rclc_support_t support = {0};
+        rcl_node_t node = rcl_get_zero_initialized_node();
+        rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+        rcl_timer_t timer = rcl_get_zero_initialized_timer();
+        rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+
+        ESP_LOGI(TAG_TASK, "Waiting for micro-ROS agent...");
+        led_status_set_state(LED_STATUS_WAITING);
+        while (rmw_uros_ping_agent(100, 5) != RMW_RET_OK) {
+            vTaskDelay(pdMS_TO_TICKS(200));
         }
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
+        ESP_LOGI(TAG_TASK, "Agent detected, starting RCL init");
+
+        RCCHECK_GOTO(rcl_init_options_init(&init_options, allocator), cleanup);
+        init_options_ready = true;
+        RCCHECK_GOTO(rcl_init_options_set_domain_id(&init_options, CONFIG_MICRO_ROS_DOMAIN_ID), cleanup);
+        RCCHECK_GOTO(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator), cleanup);
+
+        rcl_node_options_t node_ops = rcl_node_get_default_options();
+        RCCHECK_GOTO(rclc_node_init_with_options(&node, CONFIG_MICRO_ROS_NODE_NAME, "", &support, &node_ops), cleanup);
+        node_ready = true;
+
+        RCCHECK_GOTO(rclc_publisher_init_default(
+            &publisher,
+            &node,
+            ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
+            CONFIG_MICRO_ROS_TOPIC_NAME),
+            cleanup);
+        publisher_ready = true;
+
+        int configured_max = max_bin_index();
+        if (configured_max >= scan_cfg.bins) {
+            ESP_LOGE(TAG_TASK, "TOF bin map exceeds scan bins (max=%d bins=%d)",
+                     configured_max, scan_cfg.bins);
+            led_status_set_state(LED_STATUS_ERROR);
+            goto cleanup;
+        }
+
+        if (!scan_builder_init(&scan_msg, &scan_cfg)) {
+            ESP_LOGE(TAG_TASK, "scan_builder_init() failed (malloc?)");
+            led_status_set_state(LED_STATUS_ERROR);
+            goto cleanup;
+        }
+        scan_ready = true;
+
+        RCCHECK_GOTO(rclc_timer_init_default2(
+            &timer,
+            &support,
+            RCL_MS_TO_NS(TIMER_PERIOD_MS),
+            timer_callback,
+            true),
+            cleanup);
+        timer_ready = true;
+
+        RCCHECK_GOTO(rclc_executor_init(&executor, &support.context, NUMBER_OF_HANDLES, &allocator), cleanup);
+        executor_ready = true;
+        RCCHECK_GOTO(rclc_executor_add_timer(&executor, &timer), cleanup);
+
+        ESP_LOGI(TAG_TASK, "micro-ROS up: node=%s topic=%s period=%dms",
+                 CONFIG_MICRO_ROS_NODE_NAME, CONFIG_MICRO_ROS_TOPIC_NAME, TIMER_PERIOD_MS);
+        led_status_set_state(LED_STATUS_CONNECTED);
+
+        uint32_t ping_div = 0;
+        while (true) {
+            rcl_ret_t spin_ret = rclc_executor_spin_some(&executor, RCL_MS_TO_NS(50));
+            if (spin_ret != RCL_RET_OK) {
+                ESP_LOGE(TAG_TASK, "rclc_executor_spin_some() failed");
+            }
+            if ((ping_div++ % 20) == 0) {
+                if (rmw_uros_ping_agent(100, 1) != RMW_RET_OK) {
+                    ESP_LOGW(TAG_TASK, "micro-ROS agent lost, restarting session");
+                    led_status_set_state(LED_STATUS_WAITING);
+                    break;
+                }
+            }
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
 
 cleanup:
-    if (executor_ready) {
-        rc = rclc_executor_fini(&executor);
-        if (rc != RCL_RET_OK) {
-            ESP_LOGW(TAG_TASK, "rclc_executor_fini() failed: %d", (int)rc);
+        if (executor_ready) {
+            rc = rclc_executor_fini(&executor);
+            if (rc != RCL_RET_OK) {
+                ESP_LOGW(TAG_TASK, "rclc_executor_fini() failed: %d", (int)rc);
+            }
         }
-    }
-    if (timer_ready) {
-        rc = rcl_timer_fini(&timer);
-        if (rc != RCL_RET_OK) {
-            ESP_LOGW(TAG_TASK, "rcl_timer_fini() failed: %d", (int)rc);
+        if (timer_ready) {
+            rc = rcl_timer_fini(&timer);
+            if (rc != RCL_RET_OK) {
+                ESP_LOGW(TAG_TASK, "rcl_timer_fini() failed: %d", (int)rc);
+            }
         }
-    }
-    if (publisher_ready) {
-        rc = rcl_publisher_fini(&publisher, &node);
-        if (rc != RCL_RET_OK) {
-            ESP_LOGW(TAG_TASK, "rcl_publisher_fini() failed: %d", (int)rc);
+        if (publisher_ready) {
+            rc = rcl_publisher_fini(&publisher, &node);
+            if (rc != RCL_RET_OK) {
+                ESP_LOGW(TAG_TASK, "rcl_publisher_fini() failed: %d", (int)rc);
+            }
         }
-    }
-    if (node_ready) {
-        rc = rcl_node_fini(&node);
-        if (rc != RCL_RET_OK) {
-            ESP_LOGW(TAG_TASK, "rcl_node_fini() failed: %d", (int)rc);
+        if (node_ready) {
+            rc = rcl_node_fini(&node);
+            if (rc != RCL_RET_OK) {
+                ESP_LOGW(TAG_TASK, "rcl_node_fini() failed: %d", (int)rc);
+            }
         }
+        rc = rclc_support_fini(&support);
+        if (rc != RCL_RET_OK) {
+            ESP_LOGW(TAG_TASK, "rclc_support_fini() failed: %d", (int)rc);
+        }
+        if (init_options_ready) {
+            rc = rcl_init_options_fini(&init_options);
+            if (rc != RCL_RET_OK) {
+                ESP_LOGW(TAG_TASK, "rcl_init_options_fini() failed: %d", (int)rc);
+            }
+        }
+        if (scan_ready) {
+            scan_builder_deinit(&scan_msg);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
-    rc = rclc_support_fini(&support);
-    if (rc != RCL_RET_OK) {
-        ESP_LOGW(TAG_TASK, "rclc_support_fini() failed: %d", (int)rc);
-    }
-    if (scan_ready) {
-        scan_builder_deinit(&scan_msg);
-    }
-    vTaskDelete(NULL);
 }
 
 bool uros_app_start(void)
